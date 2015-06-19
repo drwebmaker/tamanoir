749e06eaa8b6f058a2ecf43acebbe1c40bfc8acc
Artem.Malieiev <artem.malieiev@gmail.com>
2015-06-19 12:39:34 +0300

    updated domains and library templates

--- src/view/domains/DomainsView.js
+++ src/view/domains/DomainsView.js
@@ -5,6 +5,8 @@ define(function (require) {
     var Backbone = require('backbone'),
         DomainsViewTemplate = require('text!template/domains/DomainsViewTemplate.html');
 
+    require('css!styles/domains/domains');
+
     return Backbone.View.extend({
         render: function () {
             this.$el.html(DomainsViewTemplate);
--- src/view/library/LibraryView.js
+++ src/view/library/LibraryView.js
@@ -5,6 +5,8 @@ define(function (require) {
     var Backbone = require('backbone'),
         LibraryViewTemplate = require('text!template/library/LibraryViewTemplate.html');
 
+    require('css!styles/library/library');
+
     return Backbone.View.extend({
         render: function () {
             this.$el.html(LibraryViewTemplate);
