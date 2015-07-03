ee8a873688e430af5c5215edee252835e9927bfe
Artem.Malieiev <artem.malieiev@gmail.com>
2015-07-03 16:53:48 +0300

    fixed edit column styles

--- src/view/EditColumnView.js
+++ src/view/EditColumnView.js
@@ -3,6 +3,8 @@
  */
 define(function (require) {
     var Backbone = require('backbone'),
+        _ = require('underscore'),
+        $ = require('jquery'),
         EditColumnViewTemplate = require('text!template/EditColumnViewTemplate.html');
 
     require('css!styles/editColumn');
@@ -10,12 +12,37 @@ define(function (require) {
     return Backbone.View.extend({
         className: 'editColumn',
         template: _.template(EditColumnViewTemplate),
+        events: {
+            'change select[name="table"]': 'onTableChange',
+            'change select[name="column"]': 'onColumnChange',
+            'click .close': 'remove',
+            'click .save': 'save'
+        },
         initialize: function () {
-            this.render();
+            Tamanoir.metadataExplorer.getMetadata('public').then(_.bind(this.onTablesMetadataLoaded, this));
         },
         render: function () {
             this.$el.html(this.template(this.model.toJSON()));
             return this;
+        },
+        onTablesMetadataLoaded: function (metadata) {
+            this.tables = metadata.items;
+            this.render();
+        },
+        onTableChange: function (event) {
+            this.table = $(event.target).val();
+            Tamanoir.metadataExplorer.getMetadata('public.' + this.table).then(_.bind(this.onColumnsMetadataLoaded, this));
+        },
+        onColumnChange: function () {
+            this.column = $(event.target).val();
+        },
+        onColumnsMetadataLoaded: function (metadata) {
+            this.columns = metadata.items;
+            this.render();
+        },
+        save: function () {
+            console.log('save clicked', this.table, this.column);
+            this.remove();
         }
     });
 });
