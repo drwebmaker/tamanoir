74e2baeaa1753b05f2e20d06c6acee4d8542ea01
Artem.Malieiev <artem.malieiev@gmail.com>
2015-07-22 18:36:14 +0300

    added simple layout algorytm

--- src/view/DataCanvasItemView.js
+++ src/view/DataCanvasItemView.js
@@ -15,7 +15,8 @@ define(function (require) {
         template: _.template(DataCanvasItemViewTemplate),
         events: {
             'click .remove': 'onRemoveClick',
-            'click input': 'onColumnClick'
+            'click input': 'onColumnClick',
+            'click .title': 'onTitleClick'
         },
         attributes: function () {
             return {
@@ -28,38 +29,8 @@ define(function (require) {
         },
         render: function () {
             this.$el.html(this.template(this.model.toJSON()));
-            this.positionalize();
 
             return this;
-        },
-        positionalize: function () {
-            this.$el.css({
-                top: this.model.get('position').top - 12,
-                left: this.model.get('position').left - 75
-            });
-        },
-        drawRelation: function () {
-            var tableName = this.model.get('name'),
-                relatedTable = this.model.get('relatedTable');
-
-            jsPlumb.draggable(tableName);
-
-            //jsPlumb.addEndpoint(tableName, {
-            //    isSource: true,
-            //    isTarget: true,
-            //    anchors: ['Top', 'Left', 'Bottom', 'Right'],
-            //    endpoint: 'Rectangle',
-            //    connector: 'Flowchart',
-            //    paintStyle:{ fillStyle: "#008CBA" },
-            //    connectorStyle:{ strokeStyle:"#008CBA", lineWidth: 3 }
-            //});
-
-            //jsPlumb.connect({
-            //    source: tableName,
-            //    target: relatedTable,
-            //    anchor: ['Left', 'Right', 'Top', 'Bottom'],
-            //    connector: 'Straight'
-            //});
         },
         onRemoveClick: function (event) {
             console.log('remove', this.model);
@@ -79,6 +50,10 @@ define(function (require) {
             }
 
             console.log('selected', this.model.get('selected'));
+        },
+        onTitleClick: function () {
+            this.$('ul').toggleClass('hide');
+            this.$el.toggleClass('z2');
         }
     });
 });
--- src/view/DataCanvasView.js
+++ src/view/DataCanvasView.js
@@ -21,26 +21,117 @@ define(function (require) {
             this._subviews = [];
 
             this.listenTo(Tamanoir, 'tables:table:dragstart', this.onSidebarTableDragStart);
-            this.listenTo(this.collection, 'add', this.addItem);
+            this.listenTo(this.collection, 'update', this.render);
             this.listenTo(this.collection, 'reset', this.render);
         },
         render: function () {
             this.$el.html(this.template);
+
+            this.plumbInstance = jsPlumb.getInstance();
+
+            this.collection.each(this.resetMetadata, this);
             this.collection.each(this.addItem, this);
 
             this.calculateHeight();
             return this;
         },
+        resetMetadata: function (model) {
+            model.set({
+                availablePlace: {top: true, bottom: true, left: true, right: true},
+                position: {top: 0, left: 0}
+            });
+        },
         addItem: function (model, index) {
             console.log('add canvas item', model);
 
-            var itemView = new DataCanvasItemView({model: model});
+            var itemView = new DataCanvasItemView({model: model}),
+                tableName = model.get('name'),
+                relatedTableName = model.get('relatedTable'),
+                relatedTable = this.collection.get(relatedTableName),
+                relatedTablePosition,
+                relatedTableAvailablePlace,
+                anchors;
 
             this._subviews.push(itemView);
 
             this.$('.canvas-items-holder').append(itemView.$el);
 
-            itemView.drawRelation();
+            if (relatedTable) {
+                relatedTablePosition = relatedTable.get('position');
+                relatedTableAvailablePlace = relatedTable.get('availablePlace');
+
+                if (relatedTableAvailablePlace.right) {
+                    anchors = ['Right', 'Left'];
+                    relatedTableAvailablePlace.right = false;
+
+                    model.set({
+                        position: {
+                            top: relatedTablePosition.top,
+                            left: relatedTablePosition.left + 300
+                        }
+                    }, {silent: true});
+
+                    relatedTable.set({
+                        availablePlace: relatedTableAvailablePlace
+                    }, {silent: true});
+
+                    itemView.$el.css(model.get('position'));
+                } else if (relatedTableAvailablePlace.bottom) {
+                    anchors = ['Bottom', 'Top'];
+                    relatedTableAvailablePlace.bottom = false;
+
+                    model.set({
+                        position: {
+                            top: relatedTablePosition.top + 100,
+                            left: relatedTablePosition.left
+                        }
+                    }, {silent: true});
+
+                    relatedTable.set({
+                        availablePlace: relatedTableAvailablePlace
+                    }, {silent: true});
+
+                    itemView.$el.css(model.get('position'));
+                } else if (relatedTableAvailablePlace.left) {
+                    anchors = ['Left', 'Right'];
+                    relatedTableAvailablePlace.left = false;
+
+                    model.set({
+                        position: {
+                            top: relatedTablePosition.top,
+                            left: relatedTablePosition.left - 300
+                        }
+                    }, {silent: true});
+
+                    relatedTable.set({
+                        availablePlace: relatedTableAvailablePlace
+                    }, {silent: true});
+
+                    itemView.$el.css(model.get('position'));
+                }
+            } else {
+                model.set({
+                    position: {
+                        top: 50,
+                        left: 50
+                    }
+                }, {silent: true});
+
+                itemView.$el.css(model.get('position'));
+            }
+
+            (function (tableName, relatedTableName) {
+                setTimeout(function () {
+                    this.plumbInstance.connect({
+                        target: tableName,
+                        source: relatedTableName,
+                        anchors: anchors,
+                        connector: 'Straight',
+                        paintStyle:{ strokeStyle:"#008CBA", lineWidth: 3},
+                        endpointStyle:{ fillStyle:"#008CBA", radius: 3}
+                    });
+                }.bind(this), 0);
+            }.bind(this)(tableName, relatedTableName));
 
             return this;
         },
@@ -57,7 +148,9 @@ define(function (require) {
             console.log('drop', this.draggedTableModel);
             Tamanoir.connecion.getColumns(this.draggedTableModel.get('name')).then(function (columns) {
                 this.collection.add(_.extend(this.draggedTableModel.toJSON(), {
-                    columns: _.map(columns, function (value) { return value.name; }),
+                    columns: _.map(columns, function (value) {
+                        return value.name;
+                    }),
                     position: {top: event.originalEvent.offsetY, left: event.originalEvent.offsetX}
                 }));
             }.bind(this));
