297312c9cf9621dd9ce88dfafc2320574a936ae6
amalieiev <brutal1ty>
2015-09-07 15:30:08 +0300

    Merge branch 'develop' into refactoring
    # Conflicts:
    #	src/collection/DataCanvasItemsCollection.js
    #	src/model/DataCanvasItemModel.js
    #	test/TablesViewSpec.js

--- src/domain/collection/TablesCollection.js
+++ src/domain/collection/TablesCollection.js
@@ -10,6 +10,66 @@ define(function (require) {
     require('backbone.localStorage');
 
     return Backbone.Collection.extend({
-        model: TableModel
+        model: TableModel,
+
+        getColumns: function () {
+            var columns = [];
+            _.each(this.toJSON(), function (value) {
+                columns = columns.concat(value.selected);
+            }, this);
+
+            //if no one is selected then return whole list
+            if (!columns.length) {
+                this.each(function (canvasItem) {
+                    columns = columns.concat(_.map(canvasItem.getColumns(), function (column) {
+                        return canvasItem.get('name') + '."' + column.name + '"';
+                    }));
+                }, this);
+            }
+
+            return columns;
+        },
+        getTables: function () {
+            return this.map(function (value) {
+                return value.get('name');
+            });
+        },
+        getConditions: function () {
+            var conditions = [];
+            if (this.size() < 2) {
+                return conditions;
+            }
+
+            var relations = [];
+            var tableNames = this.getTables();
+
+            this.each(function (model) {
+                var relatedTableNames = _.intersection(model.getRelatedTableNames(), tableNames);
+                _.each(relatedTableNames, function (relatedTableName) {
+                    relations.push(model.getRelationForTable(relatedTableName));
+                });
+            }, this);
+
+            _.each(relations, function (relation) {
+                conditions.push(relation.sourceTableName + '."' + relation.sourceColumnName + '" = ' + relation.targetTableName + '."' + relation.targetColumnName + '"');
+            });
+
+            return conditions;
+        },
+        getDataCanvasModel: function() {
+            var model = {
+                node: [],
+                edges: []
+            };
+            this.each(function(item) {
+                model.node.push({id: item.get('name'), label: item.get('name')});
+                _.each(item.get('items'), function(column) {
+                    if(column.referenceTo) {
+                        model.edges.push({from: item.get('name'), to: item._getRelatedTableName(column)});
+                    }
+                });
+            });
+            return model;
+        }
     });
 });
--- test/unit/TablesCollectionSpec.js
+++ test/unit/TablesCollectionSpec.js
@@ -5,139 +5,138 @@ define(function (require) {
 
     var DataCanvasItemsCollection = require('domain/collection/TablesCollection');
 
-    describe('TablesView', function () {
-
-        it('Should created new model', function() {
-            var metadata = [
-                {
-                    "items": [
+    describe('TablesCollection', function () {
+        describe('getDataCanvasModel method', function () {
+            it('Should generate required model for vis.js library', function () {
+                var tablesCollection = new DataCanvasItemsCollection([
                         {
-                            "referenceTo": "public.orders.OrderID",
-                            "name": "OrderID"
+                            "items": [
+                                {
+                                    "referenceTo": "public.orders.OrderID",
+                                    "name": "OrderID"
+                                },
+                                {
+                                    "referenceTo": "public.products.ProductID",
+                                    "name": "ProductID"
+                                },
+                                {
+                                    "name": "UnitPrice"
+                                },
+                                {
+                                    "name": "Quantity"
+                                },
+                                {
+                                    "name": "Discount"
+                                }
+                            ],
+                            "name": "order_details"
                         },
                         {
-                            "referenceTo": "public.products.ProductID",
-                            "name": "ProductID"
+                            "items": [
+                                {
+                                    "name": "OrderID"
+                                },
+                                {
+                                    "referenceTo": "public.customers.CustomerID",
+                                    "name": "CustomerID"
+                                },
+                                {
+                                    "referenceTo": "public.employees.EmployeeID",
+                                    "name": "EmployeeID"
+                                },
+                                {
+                                    "name": "OrderDate"
+                                },
+                                {
+                                    "name": "RequiredDate"
+                                },
+                                {
+                                    "name": "ShippedDate"
+                                },
+                                {
+                                    "name": "ShipVia"
+                                },
+                                {
+                                    "name": "Freight"
+                                },
+                                {
+                                    "name": "ShipName"
+                                },
+                                {
+                                    "name": "ShipAddress"
+                                },
+                                {
+                                    "name": "ShipCity"
+                                },
+                                {
+                                    "name": "ShipRegion"
+                                },
+                                {
+                                    "name": "ShipPostalCode"
+                                },
+                                {
+                                    "name": "ShipCountry"
+                                }
+                            ],
+                            "name": "orders"
                         },
                         {
-                            "name": "UnitPrice"
-                        },
-                        {
-                            "name": "Quantity"
-                        },
-                        {
-                            "name": "Discount"
+                            "items": [
+                                {
+                                    "name": "CustomerID"
+                                },
+                                {
+                                    "name": "CompanyName"
+                                },
+                                {
+                                    "name": "ContactName"
+                                },
+                                {
+                                    "name": "ContactTitle"
+                                },
+                                {
+                                    "name": "Address"
+                                },
+                                {
+                                    "name": "City"
+                                },
+                                {
+                                    "name": "Region"
+                                },
+                                {
+                                    "name": "PostalCode"
+                                },
+                                {
+                                    "name": "Country"
+                                },
+                                {
+                                    "name": "Phone"
+                                },
+                                {
+                                    "name": "Fax"
+                                }
+                            ],
+                            "name": "customers"
                         }
-                    ],
-                    "name": "order_details"
-                },
-                {
-                    "items": [
-                        {
-                            "name": "OrderID"
-                        },
-                        {
-                            "referenceTo": "public.customers.CustomerID",
-                            "name": "CustomerID"
-                        },
-                        {
-                            "referenceTo": "public.employees.EmployeeID",
-                            "name": "EmployeeID"
-                        },
-                        {
-                            "name": "OrderDate"
-                        },
-                        {
-                            "name": "RequiredDate"
-                        },
-                        {
-                            "name": "ShippedDate"
-                        },
-                        {
-                            "name": "ShipVia"
-                        },
-                        {
-                            "name": "Freight"
-                        },
-                        {
-                            "name": "ShipName"
-                        },
-                        {
-                            "name": "ShipAddress"
-                        },
-                        {
-                            "name": "ShipCity"
-                        },
-                        {
-                            "name": "ShipRegion"
-                        },
-                        {
-                            "name": "ShipPostalCode"
-                        },
-                        {
-                            "name": "ShipCountry"
-                        }
-                    ],
-                    "name": "orders"
-                },
-                {
-                    "items": [
-                        {
-                            "name": "CustomerID"
-                        },
-                        {
-                            "name": "CompanyName"
-                        },
-                        {
-                            "name": "ContactName"
-                        },
-                        {
-                            "name": "ContactTitle"
-                        },
-                        {
-                            "name": "Address"
-                        },
-                        {
-                            "name": "City"
-                        },
-                        {
-                            "name": "Region"
-                        },
-                        {
-                            "name": "PostalCode"
-                        },
-                        {
-                            "name": "Country"
-                        },
-                        {
-                            "name": "Phone"
-                        },
-                        {
-                            "name": "Fax"
-                        }
-                    ],
-                    "name": "customers"
-                }
-            ];
-            var collection = new DataCanvasItemsCollection(metadata);
-            var nodeEdges = {
-                node: [
-                    {id: 'order_details', label: 'order_details'},
-                    {id: 'orders', label: 'orders'},
-                    {id: 'customers', label: 'customers'}
+                    ]),
+                    expectedModel = {
+                        node: [
+                            {id: 'order_details', label: 'order_details'},
+                            {id: 'orders', label: 'orders'},
+                            {id: 'customers', label: 'customers'}
 
-                ],
-                edges: [
-                    {from: 'order_details', to: 'orders'},
-                    {from: 'order_details', to: 'products'},
-                    {from: 'orders', to: 'customers'},
-                    {from: 'orders', to: 'employees'}
-                ]
-            };
+                        ],
+                        edges: [
+                            {from: 'order_details', to: 'orders'},
+                            {from: 'order_details', to: 'products'},
+                            {from: 'orders', to: 'customers'},
+                            {from: 'orders', to: 'employees'}
+                        ]
+                    };
 
 
-            expect(collection.getDataCanvasModel()).toEqual(nodeEdges);
+                expect(tablesCollection.getDataCanvasModel()).toEqual(expectedModel);
+            });
         });
-
     });
 });
