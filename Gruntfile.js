c6f4989a1b8ca934b51346338ab2a99625123c98
amalieiev <brutal1ty>
2015-09-10 15:44:54 +0300

    added grunt-contrib-requirejs plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,33 @@
  * Created by Artem.Malieiev on 6/10/2015.
  */
 module.exports = function (grunt) {
+    require('time-grunt')(grunt);
     require('load-grunt-tasks')(grunt);
 
     grunt.initConfig({
         karma: {
             unit: {
                 configFile: 'karma.config.js',
-                autoWatch: true
+                autoWatch: false,
+                singleRun: true
+            }
+        },
+
+        requirejs: {
+            compile: {
+                options: {
+                    baseUrl: 'src',
+                    mainConfigFile: 'src/require.config.js',
+                    name: 'bower_components/almond/almond',
+                    insertRequire: ['main'],
+                    include: ['main'],
+                    out: 'build/app.js'
+                }
             }
         }
     });
 
     grunt.registerTask('test', ['karma']);
+    grunt.registerTask('build', ['requirejs']);
+    grunt.registerTask('default', ['test', 'build']);
 };
